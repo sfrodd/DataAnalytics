e7d484842d26cfc8c2e95b71559849bf5fdb8ee9
admin <sfroddgit@git.edu>
2022-07-26 14:34:02 +0530

    Feature 1 Committed

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import numpy as np
 
+def addNewFeature(msg):
+    a,b=7,9
+    if(a>b):
+        print('A Bigger'+msg)
+    else:
+        print('B Gigger'+msg)
+
 
 if __name__ == '__main__':
    dict_A={
@@ -8,7 +15,7 @@ if __name__ == '__main__':
        'Age':[19,np.nan,21,22,np.nan],
        'Sem':[3,5,np.nan,4,3]
    }
-
+   addNewFeature("Hello")
    df=pd.DataFrame(dict_A)
    print(df.isnull().sum())
    values={'Name':'Bishnoi','Age':20,'Sem':4}
