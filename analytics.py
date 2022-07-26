5b182ce8bad08e0796fa3db80d933d4e3df04098
sfrodd <sfroddgit@git.edu>
2022-07-26 11:01:23 +0530

    Add files via upload

---
+++ analytics.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+   dict_A={
+       'Name':['Ajay',np.nan,'Kiran','Vijay','Sharada'],
+       'Age':[19,np.nan,21,22,np.nan],
+       'Sem':[3,5,np.nan,4,3]
+   }
+
+   df=pd.DataFrame(dict_A)
+   print(df.isnull().sum())
+   values={'Name':'Bishnoi','Age':20,'Sem':4}
+   df.fillna(values,inplace=True)
+   df.replace(to_replace=np.nan,value=20,inplace=True)
+   df.interpolate(method='linear',limit_direction='forward',inplace=True)
+   print(df)
