7d08742f75910ae7669be3b22454e7407dff6a22
sfrodd <sfroddgit@git.edu>
2022-07-26 14:12:02 +0530

    Update analytics.py
    Updated a function

--- analytics.py
+++ analytics.py
@@ -2,8 +2,13 @@ import pandas as pd
 import numpy as np
 
 def newFeature(msg):
+   let a=7
    print("Ajay we are starting a new Starup jFork Technologies Services "+msg)
-
+   if a>5:
+      print(a)
+   else:
+      print("Less than 5 years")
+      
 if __name__ == '__main__':
    dict_A={
        'Name':['Ajay',np.nan,'Kiran','Vijay','Sharada'],
